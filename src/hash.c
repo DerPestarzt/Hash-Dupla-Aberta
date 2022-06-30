0514b652a241013baea4c0f132beb2c3b0b46c99
DerPestarzt <darkraigb@gmail.com>
2022-06-29 22:37:48 -0300

    Hash-Dupla-Fechada

---
+++ src/hash.c
@@ -0,0 +1,57 @@
+#include "hash.h"
+
+
+void Initialize(HashTable *h, int M){
+	h->table = (DataTable*) malloc (M * sizeof(DataTable));
+
+	for(int i=0; i<M; i++){
+		h->table[i].key   = -1;
+		h->table[i].value = 0;
+	}
+
+	h->M = M;
+}
+
+
+void Imprime(HashTable *h){
+	for(int i=0; i<h->M; i++)
+		printf("KEY:%d - VALUE:%d\n", h->table[i].key, h->table[i].value);
+	printf("\n");
+}
+
+
+int getValue(HashTable *h, int key){
+	int idx = hash(key, h->M);
+	int aux = idx;
+	
+	while (h->table[idx].key != key){
+		idx = (idx + 1) % h->M;
+		if (idx == aux){
+			return -1;
+		}
+	}
+
+	return h->table[idx].value;
+}
+
+void Insert(HashTable *h, int key, int value){
+	int idx = hash(key, h->M);
+	int aux = idx;
+	// int aux2;
+	
+	while (h->table[idx].key != -1){
+		// aux2 = idx;
+		idx = hash2(key, idx, h->M);
+		// if (aux2 == idx){
+		// 	idx = hash((idx+1),h->M);
+		// }
+		if (idx == aux){
+			printf("OPENED HASH IS FULL!\n");
+			return;
+		}
+	}	
+	
+	h->table[idx].key   = key;
+	h->table[idx].value = value;
+}
+
